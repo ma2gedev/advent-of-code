463230a5ab7a86b80b6e92ea126f82a28576a2fe
Takayuki Matsubara <takayuki.1229@gmail.com>
2019-12-01 17:51:05 +0900

    add solution for second

--- 2019/day01/aoc_rust/src/main.rs
+++ 2019/day01/aoc_rust/src/main.rs
@@ -2,10 +2,24 @@ use std::fs;
 
 fn main() -> std::io::Result<()> {
     let input = fs::read_to_string("../resources/input.txt")?;
-    let results: i32 = input.lines().map(|line| {
-        let num: i32 = line.parse().unwrap();
-        num / 3 - 2
+    let first_result: i32 = input.lines().map(|line| {
+        calculate(line.parse().unwrap())
     }).sum();
-    println!("First: {:?}", results);
+
+    let second_result: i32 = input.lines().map(|line| {
+        let mut result = 0;
+        let mut calc = calculate(line.parse().unwrap());
+        while calc > 0 {
+            result = result + calc;
+            calc = calculate(calc);
+        }
+        result
+    }).sum();
+
+    println!("First: {:?}\nSecond: {:?}", first_result, second_result);
     Ok(())
+}
+
+fn calculate(num: i32) -> i32 {
+    num / 3 - 2
 }
