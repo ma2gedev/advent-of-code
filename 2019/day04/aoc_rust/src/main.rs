8d98357acf03c4fc51ec571a70bea459bf0f5b3f
Takayuki Matsubara <takayuki.1229@gmail.com>
2019-12-06 20:57:47 +0900

    add day04 solution for part 1

--- 2019/day04/aoc_rust/src/main.rs
+++ 2019/day04/aoc_rust/src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+
+fn candidate(val: i32) -> bool {
+    let mut rest:i32 = val;
+    let mut prev:i32 = 200000;
+    let mut adjacent:bool = false;
+
+    while rest > 0 {
+        let current = rest % 10;
+        rest = rest / 10;
+        if current > prev {
+            return false;
+        } else if current == prev {
+            adjacent = true;
+        }
+        prev = current;
+    }
+    adjacent
+}
+
+fn main() -> std::io::Result<()> {
+    let input:Vec<i32> = fs::read_to_string("../resources/input.txt")?
+        .trim().split("-").map(|num| num.parse().unwrap()).collect();
+    let range = input[0]..=input[1];
+    //let range = 123..=144;
+
+    let values:Vec<i32> = range.filter(|i| {
+        candidate(*i)
+    }).collect();
+
+    println!("{:?}", values.len());
+
+    Ok(())
 }
