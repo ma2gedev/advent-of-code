4f248e6aa7d269abc661ed854a0ec11fba18ea91
Takayuki Matsubara <takayuki.1229@gmail.com>
2019-12-06 23:21:41 +0900

    solution for day 04 part 2

--- 2019/day04/aoc_rust/src/main.rs
+++ 2019/day04/aoc_rust/src/main.rs
@@ -18,17 +18,58 @@ fn candidate(val: i32) -> bool {
     adjacent
 }
 
+fn candidate2(val: i32) -> bool {
+    let mut rest:i32 = val;
+    let mut prev:i32 = i32::max_value();
+    let mut adjacents:[i32; 10] = [0; 10];
+
+    while rest > 0 {
+        let current = rest % 10;
+        rest = rest / 10;
+        if current > prev {
+            return false;
+        } else if current == prev {
+            adjacents[current as usize] += 1;
+        }
+        prev = current;
+    }
+    // TODO: want to shorten
+    let tmp:Vec<&i32> = adjacents.iter().filter(|&x| *x == 1).collect();
+    tmp.len() > 0
+}
+
 fn main() -> std::io::Result<()> {
     let input:Vec<i32> = fs::read_to_string("../resources/input.txt")?
         .trim().split("-").map(|num| num.parse().unwrap()).collect();
     let range = input[0]..=input[1];
-    //let range = 123..=144;
 
-    let values:Vec<i32> = range.filter(|i| {
+    let values:Vec<i32> = range.clone().filter(|i| {
         candidate(*i)
     }).collect();
 
-    println!("{:?}", values.len());
+    // first
+    println!("First {:?}", values.len());
 
+    let values2:Vec<i32> = range.filter(|i| {
+        candidate2(*i)
+    }).collect();
+
+    // second
+    println!("Second {:?}", values2.len());
     Ok(())
+}
+
+#[test]
+fn test_candidate() {
+    assert!(candidate(122345));
+    assert!(candidate(111111));
+    assert!(!candidate(223450));
+    assert!(!candidate(123789));
+}
+
+#[test]
+fn test_candidate2() {
+    assert!(candidate2(112233));
+    assert!(!candidate2(123444));
+    assert!(candidate2(111122));
 }
