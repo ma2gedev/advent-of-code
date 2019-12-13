e38c2346044ef44d4baea8c3f7bb55d6116905e9
Takayuki Matsubara <takayuki.1229@gmail.com>
2019-12-14 01:23:57 +0900

    solution for day 08 part 2

--- 2019/day08/aoc_rust/src/main.rs
+++ 2019/day08/aoc_rust/src/main.rs
@@ -3,15 +3,14 @@ use std::fs;
 fn main() -> std::io::Result<()> {
     let input: String = fs::read_to_string("../resources/input.txt")?;
     let input_str: &str = input.trim();
-    let input_num: Vec<i32> = input_str.chars().map(|num_char| {
-        num_char.to_string().parse().unwrap()
+    let input_num: Vec<u32> = input_str.chars().map(|num_char| {
+        num_char.to_digit(10).unwrap()
     }).collect();
 
     // first
     let width = 25;
     let height = 6;
     let layer_num = input_str.len() / (width * height);
-    let mut layers: Vec<Vec<Vec<i32>>> = vec![];
 
     let (min_index, _zero_counts) = (0..layer_num).map(|i| {
         let first: usize = i * width * height;
@@ -27,25 +26,26 @@ fn main() -> std::io::Result<()> {
     let two_num = (first..last).filter(|&i| input_num[i] == 2).count();
     println!("First: {:?}", one_num * two_num);
 
-    // let mut i = 0; // for
-    // while i < layer_num {
-    //     let base = i * width * height;
-    //     let mut layer = vec![];
-    //     let mut h = 0;
-    //     while h < height {
-    //         let height_base = h * width;
-    //         let mut width_line: Vec<i32> = vec![];
-    //         let mut w = 0;
-    //         while w < width {
-    //             width_line.push(input_num[base + height_base + w]);
-    //             w += 1;
-    //         }
-    //         layer.push(width_line);
-    //         h += 1;
-    //     }
-    //     layers.push(layer);
-    //     i += 1;
-    // }
+    // second
+    let mut image = vec![2; width * height];
+    image.copy_from_slice(&input_num[0..(width * height)]);
+
+    for i in 0..layer_num {
+        let first: usize = i * width * height;
+        let last: usize = (i + 1) * width * height;
+        for j in first..last {
+            match image[j - first] {
+                2 => image[j - first] = input_num[j],
+                _ => (), // do nothing
+            }
+        }
+    }
+    println!("Second:");
+    for i in 0..height {
+        let str: String = ((i * width)..((i + 1) * width))
+          .map(|num| std::char::from_digit(image[num], 10).unwrap()).collect();
+        println!("{:?}", str);
+    }
 
     Ok(())
 }
