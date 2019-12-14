0a7ab9589277363bb69e16d953fa2c4a27c9c25d
Takayuki Matsubara <takayuki.1229@gmail.com>
2019-12-14 13:37:53 +0900

    suspend mode(waiting next input) for day 07

--- 2019/day07/aoc_rust/src/main.rs
+++ 2019/day07/aoc_rust/src/main.rs
@@ -1,5 +1,5 @@
 use std::fs;
-use std::io;
+// use std::io;
 
 fn main() -> std::io::Result<()> {
     let input: Vec<i32> = fs::read_to_string("../resources/input.txt")?
@@ -13,7 +13,9 @@ fn main() -> std::io::Result<()> {
         for phase in combi {
             let mut ops = input.to_vec();
             let mut inputs = vec![phase, next_input];
-            next_input = execute(&mut ops, &mut inputs);
+            let mut outputs = vec![];
+            let (_pc, _intcode_state) = execute(&mut ops, &mut inputs, &mut outputs, 0);
+            next_input = *outputs.last().unwrap();
         }
         if max_output < next_input {
             max_output = next_input;
@@ -36,19 +38,21 @@ fn operations(ops: i32) -> (i32, i32, i32, i32) {
     (mode3, mode2, mode1, op)
 }
 
-fn from_input(inputs: &mut Vec<i32>) -> i32 {
+fn from_input(inputs: &mut Vec<i32>) -> Option<i32> {
     if inputs.len() > 0 {
-        inputs.remove(0)
+        Some(inputs.remove(0))
     } else {
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).unwrap();
-        input.trim().parse::<i32>().unwrap()
+        None
+        // from stdio for manual test
+        // let mut input = String::new();
+        // io::stdin().read_line(&mut input).unwrap();
+        // Some(input.trim().parse::<i32>().unwrap())
     }
 }
 
-fn output_to_io(value: i32) -> i32 {
+fn output_to_memory(outputs: &mut Vec<i32>, value: i32) -> () {
     println!("{:?}", value);
-    value
+    outputs.push(value)
 }
 
 fn read_value(ops: &Vec<i32>, index: usize, mode: i32) -> i32 {
@@ -60,8 +64,15 @@ fn read_value(ops: &Vec<i32>, index: usize, mode: i32) -> i32 {
     }
 }
 
-fn execute(ops: &mut Vec<i32>, inputs: &mut Vec<i32>) -> i32 {
-    let mut pc = 0;
+#[derive(PartialEq, Eq, Debug)]
+enum IntcodeState {
+    Init,
+    Halt,
+    Suspend,
+}
+
+fn execute(ops: &mut Vec<i32>, inputs: &mut Vec<i32>, outputs: &mut Vec<i32>, pc: usize) -> (usize, IntcodeState) {
+    let mut pc = pc;
     let mut op = -1; // dummy
     let mut arg1 = 0;
     let mut arg2 = 0;
@@ -70,7 +81,7 @@ fn execute(ops: &mut Vec<i32>, inputs: &mut Vec<i32>) -> i32 {
     let mut mode1 = 0;
     let mut mode2 = 0;
     let mut _mode3 = 0; // maybe unnecessary
-    let mut calculation_result = -1;
+    let mut _calculation_result = IntcodeState::Init;
 
     loop {
         if done_operation {
@@ -81,7 +92,7 @@ fn execute(ops: &mut Vec<i32>, inputs: &mut Vec<i32>) -> i32 {
         }
         match operation_step {
             0 => match operations(ops[pc]) {
-                (_, _, _, 99) => break,
+                (_, _, _, 99) => break _calculation_result = IntcodeState::Halt,
                 (parameter_mode3, parameter_mode2, parameter_mode1, o) => {
                     mode1 = parameter_mode1;
                     mode2 = parameter_mode2;
@@ -93,11 +104,14 @@ fn execute(ops: &mut Vec<i32>, inputs: &mut Vec<i32>) -> i32 {
                 1 | 2 | 5 | 6 | 7 | 8 => arg1 = read_value(ops, pc, mode1),
                 3 => {
                     let output = ops[pc] as usize;
-                    ops[output] = from_input(inputs);
+                    match from_input(inputs) {
+                        Some(o) => ops[output] = o,
+                        None => break _calculation_result = IntcodeState::Suspend,
+                    }
                     done_operation = true;
                 },
                 4 => {
-                    calculation_result = output_to_io(ops[ops[pc] as usize]);
+                    output_to_memory(outputs, ops[ops[pc] as usize]);
                     done_operation = true;
                 },
                 _ => panic!("do not reach"),
@@ -139,7 +153,10 @@ fn execute(ops: &mut Vec<i32>, inputs: &mut Vec<i32>) -> i32 {
         pc += 1;
         //println!("pc: {:?}, op: {:?}, step: {:?}, mode1: {:?}, mode2: {:?}, mode3: {:?}, arg1: {:?}, arg2: {:?}", pc, op, operation_step, mode1, mode2, _mode3, arg1, arg2);
     }
-    calculation_result
+    if _calculation_result == IntcodeState::Suspend {
+        pc -= 1; // getting from operation code for next execution
+    }
+    (pc, _calculation_result)
 }
 
 fn combination(elements: &Vec<i32>) -> Vec<Vec<i32>> {
@@ -188,5 +205,20 @@ fn test_operations() {
 #[test]
 fn test_execute() {
     let mut input: Vec<i32> = vec![3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9];
-    execute(&mut input, &mut vec![]);
+    let mut output = vec![];
+    let (_pc, _calculation_result) = execute(&mut input, &mut vec![0], &mut output, 0);
+    assert_eq!(vec![0], output);
+}
+
+#[test]
+fn test_execute_with_suspend() {
+    let mut input: Vec<i32> = vec![3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9];
+    let mut output = vec![];
+    // no input
+    let (pc, calculation_result) = execute(&mut input, &mut vec![], &mut output, 0);
+    assert_eq!(IntcodeState::Suspend, calculation_result);
+    // input zero
+    let (_pc, calculation_result) = execute(&mut input, &mut vec![0], &mut output, pc);
+    assert_eq!(vec![0], output);
+    assert_eq!(IntcodeState::Halt, calculation_result);
 }
