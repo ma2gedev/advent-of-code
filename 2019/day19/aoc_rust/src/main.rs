2a5c8f7cbc7510843e169c44eb21257f9d27798d
Takayuki Matsubara <takayuki.1229@gmail.com>
2019-12-24 23:32:40 +0900

    solution day 19 part 1

--- 2019/day19/aoc_rust/src/main.rs
+++ 2019/day19/aoc_rust/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use intcode_computer::IntcodeComputer;
+
+fn main() -> std::io::Result<()> {
+    let input: Vec<i64> = fs::read_to_string("../resources/input.txt")?
+        .trim().split(',').map(|op| op.parse().unwrap()).collect();
+    let x_max = 50;
+    let y_max = 50;
+
+    // first
+    let mut map: Vec<i64> = vec![0; x_max * y_max];
+    for x in 0..x_max {
+        for y in 0..y_max {
+            let mut outputs = vec![];
+            // need to initialize each input
+            let mut intcode = IntcodeComputer::new(&input, 0, 0);
+            let _state = intcode.execute(&mut vec![x as i64, y as i64], &mut outputs);
+            map[x % x_max + y * x_max] = *outputs.last().unwrap();
+        }
+    }
+    let tmp: Vec<&i64> = map.iter().filter(|&i| *i == 1).collect();
+
+    println!("first: {:?}", tmp.len());
+    Ok(())
 }
