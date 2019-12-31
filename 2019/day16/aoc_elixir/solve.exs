1042d05a84d106fee9f90764efda5b85c813e2bf
Takayuki Matsubara <takayuki.1229@gmail.com>
2020-01-01 01:15:47 +0900

    solution for day 16 part two

--- 2019/day16/aoc_elixir/solve.exs
+++ 2019/day16/aoc_elixir/solve.exs
@@ -10,7 +10,7 @@ defmodule Solver do
     patterns = create_patterns(length)
     (1..@phases) |> Enum.reduce(input_numbers, fn _, filtered_input ->
       [_, next_input] = patterns
-        |> Enum.reduce([filtered_input, []], fn pattern, [input_numbers, next_input] = acc ->
+        |> Enum.reduce([filtered_input, []], fn pattern, [input_numbers, next_input] ->
           result = Enum.zip(input_numbers, pattern)
             |> Enum.map(fn {num, p} -> rem(num * p, 10) end)
             |> Enum.sum
@@ -38,4 +38,25 @@ first_result = Solver.solve(input_numbers)
 
 IO.puts "first: #{first_result |> Enum.take(8) |> Enum.join}"
 
+
+# second
+defmodule Solver2 do
+  # offset is 5,970,927, number of digits is 6,500,000. so at that region fft consider less thing
+  def resolve([], _last_result) do
+    []
+  end
+  def resolve([h | tail], last_result) do
+    res = rem(h + last_result, 10)
+    [res] ++ resolve(tail, res)
+  end
+end
+
+offset = input |> String.slice(0..6) |> String.to_integer
+repeated_count = 10_000
+second_input = Stream.cycle(input_numbers) |> Enum.take(repeated_count * Enum.count(input_numbers))
+necessary_values = second_input |> Enum.slice(offset..-1)
+second_result = (1..100) |> Enum.reduce(necessary_values, fn _, filtered_input ->
+  Solver2.resolve(Enum.reverse(filtered_input), 0) |> Enum.reverse
+end)
+IO.puts "second: #{second_result |> Enum.take(8) |> Enum.join}"
 
